5f93f608abd4003c9dc644b3038df374cf813329
Etienne Palitza <e.palitza@web.de>
2025-10-17 12:57:47 +0200

    test

---
+++ MitschriftenSeminar1.R
@@ -0,0 +1,8 @@
+?sum()
+#das Fragezeichen erklärt fertige funktionen
+
+install.packages("her")
+#das muss einmal laufen, lädt herunter 
+
+library(here)
+#das muss immmer laufen, packt es in die R umgebeung
