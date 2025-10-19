fe281ff5f38109c32979b2f164034495d26ed1b1
Etienne Palitza <e.palitza@web.de>
2025-10-19 10:54:06 +0200

    Anlegen der Grundstruktur

---
+++ Scripts/LoadPackages.R
@@ -0,0 +1,29 @@
+# LoadPackages.R
+
+# Liste der benötigten Pakete
+required_packages <- c("here"#,    
+                       #"readxl"
+                       )
+
+load_packages <- function(packages) 
+{
+  # 1. Die Iteration starten
+  invisible(lapply(packages, function(pkg) {
+    
+    # 2. Prüfen, ob das Paket installiert ist
+    if (!requireNamespace(pkg, quietly = TRUE)) {
+      
+      # 3. Das Paket installieren (falls es fehlt)
+      install.packages(pkg, dependencies = TRUE)
+    }
+    
+    # 4. Das Paket laden
+    library(pkg, character.only = TRUE)
+  }))
+}
+
+# Führe die Funktion aus
+load_packages(required_packages)
+
+# Gib eine kurze Meldung aus, dass die Pakete geladen wurden
+print("Alle benötigten Pakete wurden geladen.")
