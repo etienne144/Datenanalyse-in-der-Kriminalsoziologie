dce1e453443dc0a06edd84b3069a79ff6f042e78
Etienne Palitza <e.palitza@web.de>
2026-02-23 10:47:10 +0100

    .

--- Scripts/DatensatzVorbereiten.R
+++ Scripts/DatensatzVorbereiten.R
@@ -4,53 +4,25 @@
 # Funktion zur Erstellung abgeleiteter Variablen
 # ************************************************************
 #' Führt erste notwendige Transformationen am Datensatz durch.
-#'
-#' Hauptfunktion ist die Erstellung von abgeleiteten Variablen, die für die
-#' spätere Analyse benötigt werden, indem bestehende Variablen logisch verknüpft werden.
-#' Standardmäßig arbeitet die Funktion mit dem Datensatz 'data'.
-#'
-#' @param df Data Frame: Der Eingabedatensatz, meist 'data', der vorbereitet werden soll.
-#' @return Der Data Frame mit den neu erstellten Variablen.
-#' @export
-datensatz_vorbereiten <- function(df = data)
-{
-  df_neu <- df |>
-    dplyr::mutate(
-      
-      # 1. Skalierung der "_prop" Variablen von 0-1 auf 0-100 Prozent
-      dplyr::across(
-        .cols = dplyr::ends_with("_prop"), 
-        .fns = ~ .x * 100
-      ),
-      
-      # 2. Erstellt die Variable 'bildung_hoch' als Restwert (benötigt die skalierten Werte)
-      bildung_hoch = 100 - bildung_niedrig - bildung_mittel,
-      #3 psst die Variable einkoomen an, auf einkommen je Einwohner
-      einkommen <- einkommen/1000
-    )
-  
-  cat("\nDatensatz vorbereitet\n")
-  return(df_neu) 
-}
+
 datensatz_vorbereiten_regression <- function(df = data)
 {
   df_neu <- df |>
     dplyr::mutate(
       
-      # 1. Skalierung der "_prop" Variablen von 0-1 auf 0-100 Prozent
+      # 1. Skalierung der "_prop" Variablen auf 0-1 von 0-100 Prozent
       dplyr::across(
         .cols = dplyr::ends_with("_prop"), 
         .fns = ~ .x / 100
       ),
-      # 2. Skalierung des Einkommens (Optional, aber empfohlen)
-      # Wir teilen durch 1.000. Ein 'estimate' von -0.05 bedeutet dann:
-      # "Pro 1.000 Euro mehr Einkommen sinkt die Log-Odds der AfD-Wahl um 0.05"
+      # 2. Skalierung des Einkommens, Wohnbestand
+      # 3. Erstellung der quadratischen Variabeln
       einkommen = einkommen / 1000,
       einkommen_sq = einkommen^2,
       kath_sq = kath^2,
       evang_sq = evang^2,
+      wohnbestand = wohnbestand / 1000,
       wohnbestand_sq = wohnbestand^2
-      
     )
   
   cat("\nDatensatz vorbereitet für Regression\n")
